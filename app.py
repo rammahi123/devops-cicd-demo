21db530dcd4e2337adb0f09580fa10f3dd479f4c
Ramesh <110446323+rammahi123@users.noreply.github.com>
2026-03-10 15:31:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print("CI/CD pipeline version 2")
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type','text/html')
+        self.end_headers()
+        self.wfile.write(b"Hello from Kubernetes DevOps Demo!")
+
+PORT = 80
+server = HTTPServer(("", PORT), handler)
+print("Server running on port 80...")
+server.serve_forever()
