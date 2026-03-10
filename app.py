51d465be86d9664c16d62faa58780e456462af42
Ramesh <110446323+rammahi123@users.noreply.github.com>
2026-03-10 16:42:05 +0530

    Run webserver instead of exiting

--- app.py
+++ app.py
@@ -3,11 +3,12 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type','text/html')
+        self.send_header("Content-type","text/html")
         self.end_headers()
-        self.wfile.write(b"Hello from Kubernetes DevOps Demo!")
+        self.wfile.write(b"Hello from Kubernetes CI/CD pipeline!")
 
-PORT = 80
+PORT = 5000
 server = HTTPServer(("", PORT), handler)
-print("Server running on port 80...")
+
+print("Server running on port 5000")
 server.serve_forever()
